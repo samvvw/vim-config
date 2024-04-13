bf8e6e82be4574a18db0c3ff900f91f0905f54e7
Sam Villegas <samvvww@gmail.com>
2024-04-13 10:31:48 -0700

    Cleanup lsp config file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,7 +16,7 @@ local lsp = require('lsp-zero').preset({
   },
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
@@ -34,16 +34,6 @@ vim.o.updatetime = 250
 
 vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
--- vim.lsp.diagnostic.on_publish_diagnostics, {
--- 		underline = true,
--- 		update_in_insert = false,
--- 		virtual_text = { spacing = 4, prefix = "🚧" },
--- 		severity_sort = true,
--- 	}
--- )
--- Diagnostics settings
-
 vim.diagnostic.config({
 virtual_text = {
 		prefix = "🚧"
@@ -55,10 +45,3 @@ virtual_text = {
 })
 
 lsp.setup()
-
--- local signs = { Error = "⛔", Warn = "⚠️", Hint = "💡", Info = "ℹ️ " }
--- 
--- for type, icon in pairs(signs) do
--- 	local hl = "DiagnosticSign" .. type
--- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
--- end
