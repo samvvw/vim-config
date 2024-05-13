0f37a98329ba31f722161a22999de16bfd0e282d
Sam Villegas <samvvww@gmail.com>
2024-05-13 09:00:07 -0700

    Add themes config

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,12 +1,42 @@
-vim.g['airline#extensions#tabline#enabled'] = 1
-vim.g['onedark_termcolors'] = 256
-vim.g['airline_theme'] = 'simple'
-vim.o.background = 'dark'
-vim.cmd([[colorscheme onedark]])
-
-require('onedark').setup {
-  style = 'darker'
-}
-
 require('onedark').load()
 
+function ChangeTheme(color)
+   vim.g['airline#extensions#tabline#enabled'] = 1
+
+   vim.g['onedark_termcolors'] = 256
+
+   local theme = color or vim.o.background
+
+   require("rose-pine").setup({
+          before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+         if highlight.undercurl then
+      --       highlight.undercurl = false
+         end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end
+   })
+
+   if (theme == 'light') then
+      vim.o.background = 'dark'
+      vim.g['airline_theme'] = 'simple'
+     -- vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme rose-pine-main]])
+      --   require('onedark').setup {
+      --      style = 'darker'
+      --   }
+      --   require('onedark').load()
+   else
+      vim.o.background = 'light'
+
+      vim.cmd([[colorscheme rose-pine-dawn]])
+     -- vim.cmd([[colorscheme tokyonight-day]])
+      vim.g['airline_theme'] = 'solarized'
+   end
+end
+
+ChangeTheme('light')
--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 require('sam')
 require('plugins')
+
+function SourceConfig()
+    local files = vim.fn.glob('~/.config/nvim/autoload/*')
+
+    for _, f in ipairs(vim.split(files, '\n')) do
+        vim.cmd('source ' .. f)
+    end
+
+    dofile(vim.fn.expand('$MYVIMRC'))
+end
